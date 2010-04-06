a1d697c5905629fe0a007f044a95959ebd8c6cbd
RobFE <robert.ensor@gmail.com>
2010-04-06 14:35:10 +0100

    http://storyq.codeplex.com/WorkItem/View.aspx?WorkItemId=16097, http://storyq.codeplex.com/WorkItem/View.aspx?WorkItemId=16098, http://storyq.codeplex.com/WorkItem/View.aspx?WorkItemId=16099

---
+++ src/StoryQ/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StoryQ
+{
+    internal static class StringExtensions
+    {
+        public static string Camel(this string s)
+        {
+            return Regex.Replace(" " + s, " \\w|_", match => match.Value.Trim().ToUpperInvariant());
+        }
+
+        public static string UnCamel(this string name)
+        {
+            return Regex.Replace(name, "[A-Z_]", x => " " + x.Value.ToLowerInvariant()).Trim();
+        }
+
+        public static string Join(this IEnumerable<string> strings, string seperator)
+        {
+            return string.Join(seperator, strings.ToArray());
+        }
+
+    }
+}
