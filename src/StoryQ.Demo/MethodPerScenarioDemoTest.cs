cfc31683a4dd6c1a875a66b5ec1327c9dee5ee45
RobFE <robfe@users.noreply.github.com>
2010-07-20 11:41:00 +0100

    http://storyq.codeplex.com/workitem/16100 language packs

--- src/StoryQ.Demo/MethodPerScenarioDemoTest.cs
+++ src/StoryQ.Demo/MethodPerScenarioDemoTest.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Reflection;
 using NUnit.Framework;
-using StoryQ.AllowTextualSteps;
+using StoryQ.TextualSteps;
 using StoryQ.Formatting.Parameters;
 
 namespace StoryQ.Demo
