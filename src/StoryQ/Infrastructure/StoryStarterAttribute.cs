b5f915d092554b46d45d5e4abb32f5edf99814d6
RobFE <RobFE@localhost>
2010-07-21 15:07:57 +0100

    reworked exception helper

---
+++ src/StoryQ/Infrastructure/StoryStarterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoryQ.Infrastructure
+{
+    /// <summary>
+    /// Used to mark a constructor as a valid way to kick of a story
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public class StoryStarterAttribute:Attribute
+    {
+
+        public StoryStarterAttribute(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; private set; }
+    }
+}
