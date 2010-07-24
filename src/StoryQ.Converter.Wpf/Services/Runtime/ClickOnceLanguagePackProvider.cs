c374f825d1af869ae8f675ca4b510adad30463ba
RobFE <RobFE@localhost>
2010-07-24 20:43:50 +0100

    http://storyq.codeplex.com/workitem/16100 clickonce ready?

--- src/StoryQ.Converter.Wpf/Services/Runtime/ClickOnceLanguagePackProvider.cs
+++ src/StoryQ.Converter.Wpf/Services/Runtime/ClickOnceLanguagePackProvider.cs
@@ -1,20 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Deployment.Application;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace StoryQ.Converter.Wpf.Services.Runtime
 {
     class ClickOnceLanguagePackProvider : ILanguagePackProvider
     {
+        List<AssemblyFileLanguagePack> locals = new List<AssemblyFileLanguagePack>();
+        List<RemotePack> remotes = new List<RemotePack>();
+        
+        public ClickOnceLanguagePackProvider()
+        {
+            ApplicationDeployment d = ApplicationDeployment.CurrentDeployment;
+
+            var doc = XDocument.Load(Assembly.GetEntryAssembly().Location + ".manifest");
+            var ns = XNamespace.Get("urn:schemas-microsoft-com:asm.v2");
+            var groups = doc.Root.Descendants(ns + "file").Select(x => x.Attribute("group").Value).Distinct().OrderBy(x=>x.ToLowerInvariant());
+
+            foreach (var g in groups)
+            {
+                if(d.IsFileGroupDownloaded(g))
+                {
+                    locals.Add(new AssemblyFileLanguagePack(GroupNameToDllLocation(g)));
+                }
+                else
+                {
+                    remotes.Add(new RemotePack(g));
+                }
+            }
+        }
+
+        static string GroupNameToDllLocation(string group)
+        {
+            return string.Format("LanguagePacks\\StoryQ.{0}.dll", group);
+        }
+
         public IEnumerable<ILocalLanguagePack> GetLocalLanguagePacks()
         {
             //var target = typeof(ParserEntryPointAttribute).Assembly.GetCustomAttribute<ParserEntryPointAttribute>().Target;
             yield return new EnglishLanguagePack();
+            foreach (var localProvider in locals)
+            {
+                yield return localProvider;
+            }
         }
 
         public IEnumerable<IRemoteLanguagePack> GetRemoteLanguagePacks()
         {
             //var target = typeof(ParserEntryPointAttribute).Assembly.GetCustomAttribute<ParserEntryPointAttribute>().Target;
             yield break;
+        }
+
+        
+        class RemotePack : IRemoteLanguagePack
+        {
+            readonly string groupName;
+            Downloader downloader;
+
+            public RemotePack(string groupName)
+            {
+                this.groupName = groupName;
+            }
+
+            public string Name
+            {
+                get { return groupName; }
+            }
+
+            public IEnumerable<string> CountryCodes
+            {
+                get { yield return Name.Split('-').Last(); }
+            }
+
+            public void BeginDownloadAsync(Action<double> downloadProgress, Action<ILocalLanguagePack> downloadComplete)
+            {
+                if (downloader == null)
+                {
+                    downloader = new Downloader(groupName, downloadProgress, downloadComplete);
+                }
+            }
+
+            class Downloader
+            {
+                readonly string groupName;
+                readonly Action<double> downloadProgress;
+                readonly Action<ILocalLanguagePack> downloadComplete;
+
+                public Downloader(string groupName, Action<double> downloadProgress, Action<ILocalLanguagePack> downloadComplete)
+                {
+                    this.groupName = groupName;
+                    this.downloadProgress = downloadProgress;
+                    this.downloadComplete = downloadComplete;
+
+                    var d = ApplicationDeployment.CurrentDeployment;
+                    d.DownloadFileGroupProgressChanged += CurrentDeploymentOnDownloadFileGroupProgressChanged;
+                    d.DownloadFileGroupCompleted += CurrentDeploymentOnDownloadFileGroupCompleted;
+                    d.DownloadFileGroupAsync(groupName);
+                }
+
+                void CurrentDeploymentOnDownloadFileGroupCompleted(object sender, DownloadFileGroupCompletedEventArgs args)
+                {
+                    if(args.Group == groupName)
+                    {
+                        downloadComplete(new AssemblyFileLanguagePack(GroupNameToDllLocation(groupName)));
+                    }
+                }
+
+                void CurrentDeploymentOnDownloadFileGroupProgressChanged(object sender, DeploymentProgressChangedEventArgs args)
+                {
+                    if(args.Group == groupName)
+                    {
+                        downloadProgress((double) args.BytesCompleted/args.BytesTotal);
+                    }
+                }
+            }
         }
     }
 }
--- src/StoryQ.Converter.Wpf/Services/Runtime/LocalLanguagePackProvider.cs
+++ src/StoryQ.Converter.Wpf/Services/Runtime/LocalLanguagePackProvider.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using StoryQ.Infrastructure;
 
 namespace StoryQ.Converter.Wpf.Services.Runtime
 {
@@ -14,50 +11,13 @@ namespace StoryQ.Converter.Wpf.Services.Runtime
             yield return new EnglishLanguagePack();
             foreach (var file in Directory.GetFiles("LanguagePacks", "*.dll"))
             {
-                yield return new AssemblyFile(file);
+                yield return new AssemblyFileLanguagePack(file);
             }
         }
 
         public IEnumerable<IRemoteLanguagePack> GetRemoteLanguagePacks()
         {
             return Enumerable.Empty<IRemoteLanguagePack>();
-        }
-
-        class AssemblyFile : ILocalLanguagePack
-        {
-            readonly string file;
-            object parserEntryPoint;
-
-            public AssemblyFile(string file)
-            {
-                this.file = file;
-            }
-
-            public string Name
-            {
-                get { return Path.GetFileNameWithoutExtension(file).Split('.').Last(); }
-            }
-
-            public IEnumerable<string> CountryCodes
-            {
-                get { yield return Name.Split('-').Last(); }
-            }
-
-            public object ParserEntryPoint
-            {
-                get
-                {
-                    //lazy for launch speed
-                    return parserEntryPoint ?? (parserEntryPoint = BuildEntryPoint());
-                }
-            }
-
-            object BuildEntryPoint()
-            {
-
-                var type = Assembly.LoadFile(Path.GetFullPath(file)).GetCustomAttribute<ParserEntryPointAttribute>().Target;
-                return Activator.CreateInstance(type);
-            }
         }
     }
 }
