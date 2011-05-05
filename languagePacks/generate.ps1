4e7f9fc09bd1e30d24d317c530cb16582f720992
RobFE <robfe@users.noreply.github.com>
2011-05-05 11:02:36 +0100

    Updated language pack generator script (now powershell)

--- languagePacks/generate.ps1
+++ languagePacks/generate.ps1
@@ -2,19 +2,23 @@ if(! (Test-Path bin)) {md bin}
 if(! (Test-Path obj)) {md obj}
 
 gc ..\src\StoryQ\StoryQ.flit.tt | % { $_ -replace '\$\(SolutionDir\)', "$(pwd)\" } > obj\StoryQ.flit.tt
+$tt = $env:CommonProgramFiles+'\Microsoft Shared\TextTemplating\10.0\TextTransform.exe'
+$csc = $env:windir+'\Microsoft.NET\Framework\v3.5\Csc.exe'
 
 foreach ($file in dir src)
 {
     Write-Host $file
     $fileNoExt = $file.BaseName
     echo "- Generating obj\StoryQ.flit.$fileNoExt code from $fileNoExt"    
-	& 'C:\Program Files (x86)\Common Files\Microsoft Shared\TextTemplating\1.2\TextTransform.exe' -P ..\lib\development -a !!nameSpace!StoryQ.$fileNoExt -a !!inputFile!src\$file -out obj\StoryQ.flit.$fileNoExt.g.cs obj\StoryQ.flit.tt 
+
+	& $tt -P ..\lib\development -a !!nameSpace!StoryQ.$fileNoExt -a !!inputFile!src\$file -out obj\StoryQ.flit.$fileNoExt.g.cs obj\StoryQ.flit.tt 
 
 	echo "- Generating PNG and SVG from$file - if dot is installed - www.graphviz.org"
 
 	& 'dot' -Nfontsize=10 -Efontsize=10 -Tpng "-oobj\StoryQ.$fileNoExt.png" src\$file
 	& 'dot' -Nfontsize=10 -Efontsize=10 -Tsvg "-oobj\StoryQ.$fileNoExt.svg" src\$file
 
-	#echo - Compiling bin\StoryQ.$fileNoExt.dll from obj\StoryQ.flit.$fileNoExt.g.cs
-	& 'C:\Windows\Microsoft.NET\Framework\v3.5\Csc.exe' /nologo /noconfig /doc:bin\StoryQ.$fileNoExt.XML /reference:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\v3.5\System.Core.dll" /reference:C:\Windows\Microsoft.NET\Framework\v2.0.50727\System.dll /reference:..\src\StoryQ\bin\Release\StoryQ.dll /out:bin\StoryQ.$fileNoExt.dll /target:library obj\StoryQ.flit.$fileNoExt.g.cs ..\src\StoryQ\Properties\AssemblyInfo.cs
+    #todo: use psake to compile this
+	echo - Compiling bin\StoryQ.$fileNoExt.dll from obj\StoryQ.flit.$fileNoExt.g.cs
+	& $csc /nologo /noconfig /doc:bin\StoryQ.$fileNoExt.XML /reference:"$env:ProgramFiles\Reference Assemblies\Microsoft\Framework\v3.5\System.Core.dll" /reference:$env:windir\Microsoft.NET\Framework\v2.0.50727\System.dll /reference:..\src\StoryQ\bin\Release\StoryQ.dll /out:bin\StoryQ.$fileNoExt.dll /target:library obj\StoryQ.flit.$fileNoExt.g.cs ..\src\StoryQ\Properties\AssemblyInfo.cs
 }
