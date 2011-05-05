5ca6e1325984bfb26f96544c754fc2613dbee15a
RobFE <robfe@users.noreply.github.com>
2011-05-05 10:10:02 +0100

    Improving performance by buffering output to Console.Out from the TextRenderer

---
+++ src/StoryQ.Demo/PerformanceTest.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using StoryQ.Formatting.Parameters;
+using StoryQ.TextualSteps;
+
+#if NUNIT
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+using Category = NUnit.Framework.CategoryAttribute;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+namespace StoryQ.Demo
+{
+    [TestClass]
+    public class PerformanceTest
+    {
+        [TestMethod]
+        [Category("Performance")]
+        public void Test1()
+        {
+
+            Outcome outcome = new Story("Blah")
+                .InOrderTo("Blah")
+                .AsA("Blah")
+                .IWant("Blah")
+                .WithScenario("Blah")
+                .Given(TheMethodToCall)
+                .When(TheMethodToCall)
+                .Then(TheMethodToCall);
+
+            using (new CodeTimer("creating 1000 scenarios"))
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    outcome = outcome.WithScenario("Blah " + i)
+                        .Given(TheMethodToCall)
+                        .When(TheMethodToCall)
+                        .Then(TheMethodToCall);
+                }
+            }
+            using (new CodeTimer("running 1000 scenarios"))
+            {
+                outcome.Execute();
+            }
+        }
+
+        void TheMethodToCall()
+        {
+
+        }
+    }
+
+    public class CodeTimer : IDisposable
+    {
+        private readonly string _message;
+        private readonly Stopwatch _sw = new Stopwatch();
+        public CodeTimer(string message)
+        {
+            _message = message;
+            _sw.Start();
+        }
+
+        public void Dispose()
+        {
+            _sw.Stop();
+            Console.Out.WriteLine("{0} took {1} msec", _message, _sw.ElapsedMilliseconds);
+        }
+    }
+
+}
